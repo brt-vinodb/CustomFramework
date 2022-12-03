2ec2445f9f11d6e9e1a002b11a983ce88914c224
Tejas <tejasd@bigrattle.com>
2022-12-03 23:11:00 +0530

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,13 @@
+//
+//  SDK.swift
+//  CustomFrameWorkDemoApp
+//
+//  Created by MacUser1 on 03/12/22.
+//
+
+import Foundation
+public struct SDK {
+    static func sayHello() {
+        debugPrint("Hello from SDK!")
+    }
+}
